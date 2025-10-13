d7592013b8e21ec720ab7e05d83a0f1c10097ef9
Dahlia <dahlia@sysnet24.cs.williams.edu>
2025-10-13 19:57:12 +0000

    added small tweaks

--- mnar_in_flwr/myclient/run_simulations.sh
+++ mnar_in_flwr/myclient/run_simulations.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-for ((i = 5000 ; i < 5001 ; i+=500)); do
-    echo $i > number.txt
-    flwr run . local-simulation$i
+for ((j = 1; j < 500; j+=1)); do
+    for ((i = 500 ; i < 5001 ; i+=500)); do
+	echo $i > number.txt
+
+	echo "MISSING = True" > myclient/missing.py
+	echo "COMPUTE_WEIGHTS = True" > myclient/compute.py
+	flwr run . local-simulation$i
+
+	#echo "COMPUTE_WEIGHTS = False" > myclient/compute.py
+	#flwr run . local-simulation$i
+
+	#echo "MISSING = False" > myclient/missing.py
+	#flwr run . local-simulation$i
+    done
 done
