6288c4e24d3e40ccf1e038975406d0fa4bd33ac2
Dahlia <dahlia@sysnet24.cs.williams.edu>
2025-11-20 18:29:24 +0000

    push before implementing cifar

--- mnar_in_flwr/myclient/myclient/client_app.py
+++ mnar_in_flwr/myclient/myclient/client_app.py
@@ -3,7 +3,7 @@
 import flwr
 import torch
 from flwr.client import NumPyClient, Client, ClientApp
-from flwr.common import Metrics, Context, ConfigRecord, RecordDict
+from flwr.common import Metrics, Context, ConfigRecord, RecordDict, Config
 from .knobs import DEVICE
 from .net import Net, train, test, set_parameters, get_parameters
 from torch.utils.data import Dataset, DataLoader
@@ -12,6 +12,9 @@ import pandas as pd
 import numpy as np
 from scipy.special import expit
 from .demographics import demographic_dict
+from .mnist_train import in_data
+from .mnist_test import in_test
+from random import sample
 
 # Define Flower Client and client_fn
 class IntermediateDataset(Dataset):
@@ -37,46 +40,50 @@ class MyClient(NumPyClient):
 
 
         self.demographics = {}
-        if "demographics" in self.client_state.config_records:
-            self.demographics["D1"] = self.client_state.config_records["demographics"]["D1"]
-            self.demographics["D2"] = self.client_state.config_records["demographics"]["D2"]
+        #if "demographics" in self.client_state.config_records:
+        #    self.demographics["D1"] = self.client_state.config_records["demographics"]["D1"]
+        #    self.demographics["D2"] = self.client_state.config_records["demographics"]["D2"]
             
-        dataset = self._generate_set(1)
-        self.demographics = {
-            "D1": dataset.reset_index()["D1"][0],
-            "D2": dataset.reset_index()["D2"][0],
-        }
+        #dataset = self._generate_set(1)
+        #self.demographics = {
+        #    "D1": dataset.reset_index()["D1"][0],
+        #    "D2": dataset.reset_index()["D2"][0],
+        #}
 
-        self.survey_dataset = dataset[["D1", "D2", "R", "S"]]
+        self.survey_dataset = []
         self.real_demographics_set = False
         #self.response = self.survey_dataset.reset_index(drop=True)["R"][0]
         #self.satisfied = self.survey_dataset.reset_index(drop=True)["S"][0]
 
-        self.dataset = dataset[["X", "Y", "Z", "O1"]]
+        #self.dataset = dataset[["X", "Y", "Z", "O1"]]
+        self.dataset = []
 
-
-        self._generate_large_train_set(500)
+        #self._generate_large_train_set(500)
 
     def get_parameters(self,config):
         #Return parameters the local model has                                                                    
         return get_parameters(self.net)
 
     def fit(self,parameters,config):
+        print(config)
         if not self.real_demographics_set:
             self.real_demographics_set = True
-            self.demographics["D1"] = demographic_dict[config["id"]]["D1"]
-            self.demographics["D2"] = demographic_dict[config["id"]]["D2"]
+            self.demographics["D1"] = demographic_dict[config["id"]][0]
+            self.demographics["D2"] = demographic_dict[config["id"]][1]
+            self.dataset = in_data[config["id"]] 
         #Set the local model to have the global parameters                                                        
         set_parameters(self.net,parameters)
 
 
-        df = self.dataset.sample(n=20, replace=True)
-        x_train = df.drop(["O1"], axis=1).to_numpy()
-        y_train = df["O1"].to_numpy()
+        df = sample(self.dataset,1)
+        y_train = np.array([i[0] for i in df])
+        x_train = np.array([i[1] for i in df])
+        #x_train = df.drop(["O1"], axis=1).to_numpy()
+        #y_train = df["O1"].to_numpy()
 
         train_dataset = IntermediateDataset(x_train,y_train)
         trainloader = DataLoader(train_dataset,batch_size = 1)
-t        #Train model locally for one epoch                                                                        
+        #Train model locally for one epoch                                                                        
         train(self.net,trainloader,1)
 
         #Return updated parameters, number of examples used for training, dict with "metrics"                     
@@ -85,23 +92,60 @@ t        #Train model locally for one epoch
     def evaluate(self,parameters,config):
         #Set the local model to have the global parameters                                                        
         set_parameters(self.net,parameters)
-        df = self._generate_set(10).drop(["R", "S", "D1", "D2"], axis=1)
+        #df = self._generate_set(10).drop(["R", "S", "D1", "D2"], axis=1)
+        df = sample(in_test[config["id"]],5)
+        x_val = np.array([i[1] for i in df])
+        y_val = np.array([i[0] for i in df])
 
-        x_val = df.drop(["O1"], axis=1).to_numpy()
-        y_val = df["O1"].to_numpy()
+        #x_val = df.drop(["O1"], axis=1).to_numpy()
+        #y_val = df["O1"].to_numpy()
         val_dataset = IntermediateDataset(x_val,y_val)
         valloader = DataLoader(val_dataset,batch_size = 1)
         #Run our testing function                                                                                 
         loss, accuracy = test(self.net,valloader)
         #print(loss,accuracy)
-        #Return loss, num_examples, and metrics dictionary                                                        
+        #Return loss, num_examples, and metrics dictionary
+
         return loss, len(valloader), {"accuracy": float(accuracy)}
 
-    def get_properties(self, ins=None,config=None):
-        temp_dict = self._generate_set(1).iloc[0].to_dict()
-        if temp_dict["R"] == 0:
-            temp_dict["S"] = -1
-        return temp_dict
+    def get_properties(self, config):
+
+        #Actually just need to return a dataframe with R, S, D1 and D2
+        #temp_dict = self._generate_set(1).iloc[0].to_dict()
+        #if temp_dict["R"] == 0:
+        #    temp_dict["S"] = -1
+        #return temp_dict
+        n_samples = 1
+        self.dataset = in_data[config["id"]]
+        df = sample(self.dataset,1)
+        O1 = df[0][0]
+        x_train = np.array([i[1] for i in df])
+        y_train = np.array([i[0] for i in df])
+        train_dataset = IntermediateDataset(x_train,y_train)
+        trainloader = DataLoader(train_dataset,batch_size = 1)
+        inputs, targets = next(iter(trainloader))
+        O1hat = self.net(inputs).detach().numpy().flatten()
+        print("O1")
+        print(O1)
+        print("O1hat")
+        print(O1hat)
+        
+        D1, D2 = demographic_dict[config["id"]][0], demographic_dict[config["id"]][1]
+        S = np.random.binomial(1, expit(D1 - 10 * (O1 - O1hat) ** 2), n_samples)
+
+        pRS0 = expit(2 * D1)
+
+        R = np.random.binomial(
+            1, pRS0 / (pRS0 + np.exp(4 * (1 - S)) * (1 - pRS0)), n_samples
+        )
+
+
+        #print(type(S),type(R))
+        df = {"D1": D1, "D2": D2, "S": int(S[0]), "R": int(R[0])}
+        if df["R"] == 0:
+            df["S"] = -1
+
+        return df
     def _generate_large_train_set(self, num_rows):
         """                                                                                                      \
                                                                                                                   
